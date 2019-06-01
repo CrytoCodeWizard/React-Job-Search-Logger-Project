3e1ecbdfe1ca2d698e49abc49da131140f727c4e
David Teo <starrynightskyhoney@gmail.com>
2019-06-01 10:15:26 +0100

    Import bootstrap.css in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 
 import store from  './store'
