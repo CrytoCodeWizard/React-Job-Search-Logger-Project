95928e579a6b22e96a68820f392766c19d70546c
David Teo <starrynightskyhoney@gmail.com>
2019-06-01 11:13:15 +0100

    Create RoleCard component renamed to be singular, Import Card from bootstrap/Card and Add basic structure

---
+++ src/components/RoleCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+const RoleCard = (props) => {
+	return (
+		<Card style={{minWidth: '25%',maxwidth: '33%'}}>
+			<h1 style={{color: 'red'}}>This is the RoleCard component</h1>
+			<Card.Body>
+				<Card.Title>
+					<a href="#">title</a>
+					{/* <a href={"/roles/"+props.role.id}>{props.role.title}</a> */}
+				</Card.Title>
+				<Card.Subtitle>
+					<p>A subtitle</p>
+					{/* <p>{props.role.company}  <small>{props.role.location}</small></p> */}
+				</Card.Subtitle>
+				<Card.Text>
+					<small>Some text</small>
+					{/* <small>Status: {props.role.status}</small> */}
+				</Card.Text>
+			</Card.Body>
+		</Card >
+	);
+}
+
+export default RoleCard;
