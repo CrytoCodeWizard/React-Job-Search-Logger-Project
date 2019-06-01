b9b6c3e9924ad29e6e3208893f27f62c7fc541aa
David Teo <starrynightskyhoney@gmail.com>
2019-06-01 10:33:27 +0100

    Import BrowserRouter and Route from react-router-dom and components Navbar, Home, Roles, and Add components to render in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom'
 
 import './index.css';
+
+import Navbar from './components/Navbar'
+import Home from './containers/Home'
+import Roles from './containers/Roles'
 
 
 function App() {
@@ -9,6 +14,11 @@ function App() {
 			<header className="App-header">
 				Job Search Logger
       </header>
+			<Navbar />
+			<BrowserRouter>
+				<Route path="/" component={Home} />
+				<Route path="/roles" component={Roles} />
+			</BrowserRouter>
 		</div>
 	);
 }
