ebb3529df3f945cf6c56c2b6b3fb211259e9ef2d
David Teo <starrynightskyhoney@gmail.com>
2019-06-01 10:20:06 +0100

    Correct reducer name to rootReducer in store.hs

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import {createStore,applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
 
-import RolesReducer from './reducers/RolesReducer'
+import rootReducer from './reducers/rootReducer'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(RolesReducer, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(rootReducer,composeEnhancer(applyMiddleware(thunk)));
 
 export default store
