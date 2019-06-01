83abc807ebc20f5cc0e90353fb49a21a725f0cf4
David Teo <starrynightskyhoney@gmail.com>
2019-06-01 12:04:54 +0100

    Create rootReducer with a SET_ROLES case to return action.roles (roles being the payload) and default statements

---
+++ src/components/Role.jsx
@@ -0,0 +1,29 @@
+import React,{Component} from 'react'
+import Card from 'react-bootstrap/Card'
+
+class Role extends Component {
+
+	render() {
+		return (
+			<Card>
+				<h1 style={{color: 'red'}}>This is the Role component</h1>
+				<Card.Body>
+					<Card.Title>
+						<h2>A title</h2>
+						{/* <h2>{this.state.title}</h2> */}
+					</Card.Title>
+					<Card.Subtitle>
+						<p>A subtitle</p>
+						{/* <p>{this.state.company}  <small>{this.state.location}</small></p> */}
+					</Card.Subtitle>
+					<Card.Text>
+						<small>Some text</small>
+						{/* <small>Status: {this.state.status}</small> */}
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		);
+	}
+}
+
+export default Role;
