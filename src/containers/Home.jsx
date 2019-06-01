84a4df42c9355fdd24c446b55c4d290e1b69537d
David Teo <starrynightskyhoney@gmail.com>
2019-06-01 10:41:24 +0100

    Create Home component with basic structure

---
+++ src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<header className="App-header">
+			<h1>Job Search Logger</h1>
+			<p>Keep organised during your job search</p>
+		</header>
+	)
+}
+export default Home
+
+
