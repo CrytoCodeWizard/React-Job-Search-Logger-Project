08e7388d4c536845f8fcbaadfbe0ba37f48bcbe4
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:37:35 +0100

    Remove event.preventDefault from actions

--- src/components/Role.jsx
+++ src/components/Role.jsx
@@ -6,30 +6,30 @@ import {connect} from 'react-redux'
 class Role extends Component {
 
 	render() {
-		return (
-			<Card>
-				<h1 style={{color: 'red'}}>This is the Role component</h1>
-				<Card.Body>
-					<Card.Title>
-						<h2>A title</h2>
-						{/* <h2>{this.state.title}</h2> */}
-					</Card.Title>
-					<Card.Subtitle>
-						<p>A subtitle</p>
-						{/* <p>{this.state.company}  <small>{this.state.location}</small></p> */}
-					</Card.Subtitle>
-					<Card.Text>
-						<small>Some text</small>
-						{/* <small>Status: {this.state.status}</small> */}
-					</Card.Text>
-				</Card.Body>
-			</Card>
-		);
+		if(!this.props.selectedRole) {
+			return null
+		} else {
+			return (
+				<Card>
+					<h1 style={{color: 'red'}}>This is the Role component</h1>
+					<Card.Body>
+						<Card.Title>
+							<h2>{this.props.selectedRole.title}</h2>
+						</Card.Title>
+						<Card.Subtitle>
+							<p>{this.props.selectedRole.company}</p>
+						</Card.Subtitle>
+						<Card.Text>
+							<small>{this.props.selectedRole.status}</small>
+						</Card.Text>
+					</Card.Body>
+				</Card>
+			);
+		}
 	}
 }
 
 const mapStateToProps = state => {
-	console.log('@@@ state from mapStateToProps',state)
 	return {
 		selectedRole: state.selectedRole
 	}
