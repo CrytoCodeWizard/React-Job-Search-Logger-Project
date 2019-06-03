e78cba2cc2ea40ed07b84445472acecca906147c
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:08:31 +0100

    Convert functional component into class component

--- src/components/RoleCard.jsx
+++ src/components/RoleCard.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
+
 import Card from 'react-bootstrap/Card'
 
-const RoleCard = (props) => {
-	return (
-		<Card style={{minWidth: '25%',maxwidth: '33%'}}>
-			<h1 style={{color: 'red'}}>This is the RoleCard component</h1>
-			<Card.Body>
-				<Card.Title>
-					<a href="#">title</a>
-					{/* <a href={"/roles/"+props.role.id}>{props.role.title}</a> */}
-				</Card.Title>
-				<Card.Subtitle>
-					<p>A subtitle</p>
-					{/* <p>{props.role.company}  <small>{props.role.location}</small></p> */}
-				</Card.Subtitle>
-				<Card.Text>
-					<small>Some text</small>
-					{/* <small>Status: {props.role.status}</small> */}
-				</Card.Text>
-			</Card.Body>
-		</Card >
-	);
+
+class RoleCard extends React.Component {
+
+	render() {
+		return (
+			<Card style={{minWidth: '25%',maxwidth: '33%'}}>
+				<h1 style={{color: 'red'}}>This is the RoleCard component</h1>
+				<Card.Body>
+					<Card.Title>
+						<a href="#">title</a>
+						{/* <a href={"/roles/"+props.role.id}>{props.role.title}</a> */}
+					</Card.Title>
+					<Card.Subtitle>
+						<p>A subtitle</p>
+						{/* <p>{props.role.company}  <small>{props.role.location}</small></p> */}
+					</Card.Subtitle>
+					<Card.Text>
+						<small>Some text</small>
+						{/* <small>Status: {props.role.status}</small> */}
+					</Card.Text>
+				</Card.Body>
+			</Card >
+		);
+	}
 }
 
 export default RoleCard;
