a166662fb53bcbb5ff3da29240c39e828e0ba1d1
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:02:51 +0100

    Add an initialState const with empty/null roles and selectedRole

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,8 @@
-export default (state = [],action) => {
+const initialState = {
+	roles: [],
+	selectedRole: null
+}
+export default (state = initialState,action) => {
 	switch(action.type) {
 		case "SET_ROLES":
 			return action.roles
