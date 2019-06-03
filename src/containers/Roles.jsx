eee4da148cec5829e5d532b304f94c081360223c
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:45:26 +0100

    Remove Fragment and comment

--- src/containers/Roles.jsx
+++ src/containers/Roles.jsx
@@ -6,13 +6,10 @@ import Role from '../components/Role'
 
 const Roles = () => {
 	return (
-		<React.Fragment>
-			<h1 style={{color: 'red'}}>This is the Roles component</h1>
-			<CardGroup>
-				<RoleCard />
-			</CardGroup>
-			<Role />
-		</React.Fragment>
+		<CardGroup>
+			<RoleCard />
+		</CardGroup>
+		<Role />
 	)
 }
 
