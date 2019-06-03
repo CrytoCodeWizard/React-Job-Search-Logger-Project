33dc009b1d80f5a30e696f2186ddbebe93da6597
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:41:32 +0100

    Remove a comment

--- src/containers/Roles.jsx
+++ src/containers/Roles.jsx
@@ -5,7 +5,6 @@ import RoleCard from '../components/RoleCard'
 import Role from '../components/Role'
 
 const Roles = () => {
-	{/* React.Fragment AND h1 ADDED FOR VISUAL AID AND TO BE REMOVED ONCE ALL WORKS */}
 	return (
 		<React.Fragment>
 			<h1 style={{color: 'red'}}>This is the Roles component</h1>
