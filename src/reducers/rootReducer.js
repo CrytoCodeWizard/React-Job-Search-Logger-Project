bf7f46f851f6fdd7e35e85931472c25f9bd57171
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:04:04 +0100

    Add case statement for SET_ROLE to add selectedRole to state in rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -6,6 +6,8 @@ export default (state = initialState,action) => {
 	switch(action.type) {
 		case "SET_ROLES":
 			return action.roles
+		case "SET_ROLE":
+			return {...state,selectedRole: action.selectedRole}
 		default:
 			return state
 	}
