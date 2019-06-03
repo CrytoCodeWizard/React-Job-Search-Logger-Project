1bcec35b20f4e621eaaf04120527da81e2c76176
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:15:34 +0100

    Map over roles prop to render title, company, location and status within RoleCard, and Add a call to the getRoles action in componentDidMount

--- src/components/RoleCard.jsx
+++ src/components/RoleCard.jsx
@@ -5,26 +5,28 @@ import {selectRole} from '../actions/actions'
 import Card from 'react-bootstrap/Card'
 
 class RoleCard extends React.Component {
+	componentDidMount() {
+		this.props.getRoles()
+	}
 
 	render() {
 		return (
-			<Card style={{minWidth: '25%',maxwidth: '33%'}}>
-				<h1 style={{color: 'red'}}>This is the RoleCard component</h1>
-				<Card.Body>
-					<Card.Title>
-						<a href="#">title</a>
-						{/* <a href={"/roles/"+props.role.id}>{props.role.title}</a> */}
-					</Card.Title>
-					<Card.Subtitle>
-						<p>A subtitle</p>
-						{/* <p>{props.role.company}  <small>{props.role.location}</small></p> */}
-					</Card.Subtitle>
-					<Card.Text>
-						<small>Some text</small>
-						{/* <small>Status: {props.role.status}</small> */}
-					</Card.Text>
-				</Card.Body>
-			</Card >
+			this.props.roles.map(role =>
+				<Card style={{minWidth: '25%',maxwidth: '33%'}}>
+					<h1 style={{color: 'red'}}>This is the RoleCard component</h1>
+					<Card.Body>
+						<Card.Title>
+							<a href={"/roles/" + role.id} onClick={(event,role) => this.props.selectRole(event,role)}>{role.title}</a>
+						</Card.Title>
+						<Card.Subtitle>
+							<p>{role.company}  <small>{role.location}</small></p>
+						</Card.Subtitle>
+						<Card.Text>
+							<small>Status: {role.status}</small>
+						</Card.Text>
+					</Card.Body>
+				</Card >
+			)
 		);
 	}
 }
