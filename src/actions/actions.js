65ec885d0b1ca4c519355b61c814859e948e777b
David Teo <starrynightskyhoney@gmail.com>
2019-06-03 19:06:22 +0100

    Add action creator for SET_ROLE and a selectRole to dispatch a selectedRole

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,12 +1,22 @@
 export const setRoles = roles => {
-  return { type: "SET_ROLES", roles: roles }
+	return {type: "SET_ROLES",roles: roles}
 }
 
-// async
+export const setRole = role => {
+	return {type: "SET_ROLE",selectedRole: role}
+}
+
 export const getRoles = () => {
-  return dispatch => {
-    return fetch("http://localhost:3001/api/roles")
-      .then(resp => resp.json())
-      .then(roles => dispatch(setRoles(roles)))
-  }
+	return dispatch => {
+		return fetch("http://localhost:3001/api/roles")
+			.then(resp => resp.json())
+			.then(roles => dispatch(setRoles(roles)))
+	}
+}
+
+export const selectRole = (event,role) => {
+	event.preventDefault()
+	return dispatch => {
+		return dispatch(setRole(role))
+	}
 }
