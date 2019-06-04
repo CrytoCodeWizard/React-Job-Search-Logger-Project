c474c6de0d2d92413bf2e09564c73109b1dbc30d
David Teo <starrynightskyhoney@gmail.com>
2019-06-04 11:36:47 +0100

    Deconstruct props and Add them to render in a Card in Role

--- src/components/Role.jsx
+++ src/components/Role.jsx
@@ -9,17 +9,24 @@ class Role extends Component {
 		if(!this.props.selectedRole) {
 			return null
 		} else {
+			const {title,company,status,description,requirements} = this.props.selectedRole
+			const req = requirements.toString().replace("[","").replace("]","")
+			console.log(req)
 			return (
 				<Card>
 					<Card.Body>
 						<Card.Title>
-							<h2>{this.props.selectedRole.title}</h2>
+							<h2>{title}</h2>
 						</Card.Title>
 						<Card.Subtitle>
-							<p>{this.props.selectedRole.company}</p>
+							<p>{company}</p>
 						</Card.Subtitle>
 						<Card.Text>
-							<small>{this.props.selectedRole.status}</small>
+							<small>{status}</small>
+							<br />
+							{description}
+							<br /><br />
+							<strong>Requirements: </strong>{req}
 						</Card.Text>
 					</Card.Body>
 				</Card>
