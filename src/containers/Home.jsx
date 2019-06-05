e1eef76ed7791fc338416e3f3ac079bff5647c69
David Teo <starrynightskyhoney@gmail.com>
2019-06-05 15:55:02 +0100

    Comments

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
 	return (
 		<header className="App-header">
 			<h1>Job Search Logger</h1>
-			<p>Keep organised during your job search</p>
+			{/* <p>Keep organised during your job search</p> */}
 		</header>
 	)
 }
