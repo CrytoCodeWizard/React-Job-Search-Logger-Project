92b163a7332211c505378d4706c989376469344c
David Teo <starrynightskyhoney@gmail.com>
2019-06-05 15:20:43 +0100

    Deconstruct state inside render to use in input values in NewRoleForm.jsx

--- src/components/NewRoleForm.jsx
+++ src/components/NewRoleForm.jsx
@@ -23,31 +23,32 @@ class NewRoleForm extends Component {
 	}
 
 	render() {
+		const {title,description,requirements,company,location,status,url} = this.state.formData
 		return (
 			<React.Fragment>
 				This is the New Role Form component
 
 				<form onSubmit={this.handleSubmit}>
 					<label>Title</label>
-					<input name="title" placeholder="Job Title" value="" onChange={this.handleChange} />
+					<input name="title" placeholder="Job Title" value={title} onChange={this.handleChange} />
 					<br />
 					<label>Description</label>
-					<input name="description" placeholder="Job Description" value="" onChange={this.handleChange} />
+					<input name="description" placeholder="Job Description" value={description} onChange={this.handleChange} />
 					<br />
 					<label>Requirements</label>
-					<input name="requirements" placeholder="Job Requirements" value="" onChange={this.handleChange} />
+					<input name="requirements" placeholder="Job Requirements" value={requirements} onChange={this.handleChange} />
 					<br />
 					<label>Company</label>
-					<input name="company" placeholder="Company" value="" onChange={this.handleChange} />
+					<input name="company" placeholder="Company" value={company} onChange={this.handleChange} />
 					<br />
 					<label>Location</label>
-					<input name="location" placeholder="Location" value="" onChange={this.handleChange} />
+					<input name="location" placeholder="Location" value={location} onChange={this.handleChange} />
 					<br />
 					<label>Status</label>
-					<input name="status" placeholder="Status" value="" onChange={this.handleChange} />
+					<input name="status" placeholder="Status" value={status} onChange={this.handleChange} />
 					<br />
 					<label>URL</label>
-					<input name="url" placeholder="Posting url" value="" onChange={this.handleChange} />
+					<input name="url" placeholder="Posting url" value={url} onChange={this.handleChange} />
 					<br />
 					<button type="submit">Add</button>
 				</form>
