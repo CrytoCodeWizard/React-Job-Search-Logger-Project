b4b561bee33cd43bba958d2e4736aa905574a080
David Teo <starrynightskyhoney@gmail.com>
2019-06-05 14:36:58 +0100

    Comments

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -38,3 +38,9 @@ export const updateRole = (selectedRole,newValue) => {
 	}
 }
 
+
+// I also need to change the roles array in state to replace the old role with the updated role.
+// I will need to update the state in the reducer
+	// -- can I do it at the same time I process the slectedRole change or does this need to be a separate action/reducer?
+//  I will also need to fetch and PATCH the updated role to persist
+// -- This PATCH should fetch to api/roles/:id rather than api/roles
