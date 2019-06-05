3a69b59d738a7ad97f36a520ed380a62cd08392e
David Teo <starrynightskyhoney@gmail.com>
2019-06-05 15:02:27 +0100

    Add form with labels and input fields with basic structure event handlers onChange to handleChange for input and onSubmit to handleSubmit for form

--- src/components/NewRoleForm.jsx
+++ src/components/NewRoleForm.jsx
@@ -1,14 +1,48 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react';
 
 class NewRoleForm extends Component {
-	state = {  }
-	render() { 
+	state = {}
+
+	handleChange = event => {
+		console.log("handling change")
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+		console.log('Submitted!')
+	}
+
+	render() {
 		return (
-			<div>
+			<React.Fragment>
 				This is the New Role Form component
-			</div>
+
+				<form onSubmit={this.handleSubmit}>
+					<label>Title</label>
+					<input name="title" placeholder="Job Title" value="" onChange={this.handleChange} />
+					<br />
+					<label>Description</label>
+					<input name="description" placeholder="Job Description" value="" onChange={this.handleChange} />
+					<br />
+					<label>Requirements</label>
+					<input name="requirements" placeholder="Job Requirements" value="" onChange={this.handleChange} />
+					<br />
+					<label>Company</label>
+					<input name="company" placeholder="Company" value="" onChange={this.handleChange} />
+					<br />
+					<label>Location</label>
+					<input name="location" placeholder="Location" value="" onChange={this.handleChange} />
+					<br />
+					<label>Status</label>
+					<input name="status" placeholder="Status" value="" onChange={this.handleChange} />
+					<br />
+					<label>URL</label>
+					<input name="url" placeholder="Posting url" value="" onChange={this.handleChange} />
+					<br />
+					<button type="submit">Add</button>
+				</form>
+			</React.Fragment>
 		);
 	}
 }
- 
 export default NewRoleForm;
