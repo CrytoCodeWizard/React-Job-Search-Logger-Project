1ece888a848c647886cfdbef9d5f83a9ab717e94
David Teo <starrynightskyhoney@gmail.com>
2019-06-05 14:43:26 +0100

    Create NewRoleForm class component

---
+++ src/components/NewRoleForm.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewRoleForm extends Component {
+	state = {  }
+	render() { 
+		return (
+			<div>
+				This is the New Role Form component
+			</div>
+		);
+	}
+}
+ 
+export default NewRoleForm;
