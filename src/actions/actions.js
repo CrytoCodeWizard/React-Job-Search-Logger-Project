851db48851583b3320d53fdb57796df4dce156db
David Teo <starrynightskyhoney@gmail.com>
2019-06-05 14:05:11 +0100

    Add updateRoleAction and updateRole in actions to change the status value of selectedRole

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,21 +1,40 @@
-export const setRoles = roles => {
+export const setRolesAction = roles => {
 	return {type: "SET_ROLES",roles: roles}
 }
 
-export const setRole = role => {
+export const setRoleAction = role => {
 	return {type: "SET_ROLE",selectedRole: role}
 }
+
+export const updateRoleAction = role => {
+	return {type: "UPDATE_ROLE",selectedRole: role}
+}
+
+// ************
 
 export const getRoles = () => {
 	return dispatch => {
 		return fetch("http://localhost:3001/api/roles")
 			.then(resp => resp.json())
-			.then(roles => dispatch(setRoles(roles)))
+			.then(roles => dispatch(setRolesAction(roles)))
 	}
 }
 
 export const selectRole = (role) => {
 	return dispatch => {
-		return dispatch(setRole(role))
+		return dispatch(setRoleAction(role))
 	}
 }
+
+export const updateRole = (selectedRole,newValue) => {
+	return dispatch => {
+		const updatedRole = {
+			...selectedRole,
+			status: newValue
+		}
+		// console.log(">>>>1 updatedRole:  ",updatedRole)
+		//=>the selectedRole
+		return dispatch(updateRoleAction(updatedRole))
+	}
+}
+
