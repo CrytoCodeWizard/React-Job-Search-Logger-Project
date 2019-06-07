26a313edf37eb8b3369a8b97c33e3448d9351544
David Teo <starrynightskyhoney@gmail.com>
2019-06-07 11:03:40 +0100

    Update UPDATE_ROLE in reducers to find the index index in roles of the role matching selectedRole, and Update roles with .slice() to update roles state and selectedRole state with the edited selectedRole

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -8,9 +8,10 @@ export default (state = initialState,action) => {
 			return {...state,roles: action.roles}
 		case "SET_ROLE":
 			return {...state,selectedRole: action.selectedRole}
-		// case 'UPDATE_ROLE':
-		// const X = map/filter roles to find matching id to selectedRole to get new array
-		// return {...state,selectedRole: action.selectedRole, roles: X}
+		case 'UPDATE_ROLE':
+			const matchedRole = state.roles.find(r => r.id === action.selectedRole.id)
+			const index = state.roles.indexOf(matchedRole)
+			return {selectedRole: action.selectedRole,roles: [...state.roles.slice(0,index),action.selectedRole,...state.roles.slice(index + 1)]}
 		case 'ADD_ROLE':
 			return {...state,roles: action.role}
 		default:
