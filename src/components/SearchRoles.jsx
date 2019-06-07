f6c70527c1cea0e10bb3939b889c9529c74fb97c
David Teo <starrynightskyhoney@gmail.com>
2019-06-07 20:59:45 +0100

    Import Form and Button from react-bootstrap and index.css, and Add props and a form to select status and submit in SearchRoles

--- src/components/SearchRoles.jsx
+++ src/components/SearchRoles.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-const SearchRoles = () => {
+
+import '../index.css'
+
+
+const SearchRoles = ({handleSearchChange,handleSearchSubmit}) => {
+	// console.log('!!!! props.roles in SearchRoles component: ',props.roles)
+
 	return (
 		<>
-			<label>Search by status:</label>
-			<input type="select"/>
-
+			<Form onSubmit={handleSearchSubmit} onChange={(event) => handleSearchChange(event)} >
+				<Form.Label>Search by status:</Form.Label>
+				<Form.Control as="select">
+					<option value="Interested">Interested</option>
+					<option value="Applied">Applied</option>
+					<option value="Interviewing">Interviewing</option>
+					<option value="Successful">Successful</option>
+					<option value="Rejected">Rejected</option>
+					<option value="Not Interested">Not Interested</option>
+				</Form.Control>
+				<Button type="submit">Search</Button>
+			</Form>
 		</>
-		);
-	}
-	
+	);
+}
+
 export default SearchRoles;
