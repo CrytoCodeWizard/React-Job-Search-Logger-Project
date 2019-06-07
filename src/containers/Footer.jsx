5df3d4a6a003966a54eeb2196d36f3c8e773a1ea
David Teo <starrynightskyhoney@gmail.com>
2019-06-07 14:17:36 +0100

    Create a Footer component

---
+++ src/containers/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<a href='/roles'>View your saved roles</a>
+			<p>JobSearchLogger</p>
+		</footer>
+	);
+}
+
+export default Footer;
