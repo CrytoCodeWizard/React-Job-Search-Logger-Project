faa09886c3ab8164afa2475c0a82dbfc9a0a0c83
David Teo <starrynightskyhoney@gmail.com>
2019-06-07 17:29:12 +0100

    Import index.css

--- src/components/RoleCard.jsx
+++ src/components/RoleCard.jsx
@@ -4,6 +4,8 @@ import {getRoles} from '../actions/actions'
 import {selectRole} from '../actions/actions'
 import Card from 'react-bootstrap/Card'
 
+import '../index.css'
+
 import SearchRoles from './SearchRoles'
 
 class RoleCard extends React.Component {
