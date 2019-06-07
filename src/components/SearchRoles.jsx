df56188d6779cd8149f9ed4ef0eaed7b1c17d5d7
David Teo <starrynightskyhoney@gmail.com>
2019-06-07 15:17:11 +0100

    Create SearchRoles component

---
+++ src/components/SearchRoles.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchRoles = () => {
+	return (
+		<>
+			<label>Search by status:</label>
+			<input type="select"/>
+
+		</>
+		);
+	}
+	
+export default SearchRoles;
