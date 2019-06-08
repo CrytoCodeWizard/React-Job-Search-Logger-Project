b27f9662084421109636f10c5c2000a34566214b
David Teo <starrynightskyhoney@gmail.com>
2019-06-08 15:58:19 +0100

    Correct typo

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div id='home-comp'>
-			<h1>Wecome to Job Search Logger</h1>
+			<h1>Welcome to Job Search Logger</h1>
 			<p>Add job postings and keep track of their status during your job search process</p>
 			<p><strong>You can view saved roles or create new entries via the links in the navigation bar above!</strong></p>
 		</div>
