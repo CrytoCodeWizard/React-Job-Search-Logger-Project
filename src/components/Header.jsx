f565dbcca6d2ebe456d0377867cca2d9ccab397b
David Teo <starrynightskyhoney@gmail.com>
2019-06-08 15:23:28 +0100

    Create a Header component to display name and brief description of the app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className="App-header">
+			<h1>Job Search Logger</h1>
+			<p>Keep organised during your job search</p>
+		</header>
+	)
+}
+export default Header
