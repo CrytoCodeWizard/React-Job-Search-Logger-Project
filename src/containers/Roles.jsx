c6768449bbe72b713a027141f8d2d04424417b12
David Teo <starrynightskyhoney@gmail.com>
2019-06-08 16:01:43 +0100

    Add id roles-comp for styling in Roles

--- src/containers/Roles.jsx
+++ src/containers/Roles.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import CardGroup from 'react-bootstrap/CardGroup'
 
+import '../index.css'
+
 import RoleCard from '../components/RoleCard'
 import Role from '../components/Role'
 
 const Roles = () => {
 	return (
-		<div style={{margin: '5px'}}>
+		<div id="roles-comp">
 			<CardGroup>
 				<RoleCard />
 			</CardGroup>
 			<Role />
-		</div>
+		</div >
 	)
 }
 
